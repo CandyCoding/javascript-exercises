5aa2fd6f3117a0f58b295f2224ab92ee94fb5ea5
CandyL <candy.lopez.gr@gmail.com>
2026-06-19 17:09:29 -0600

    Ejercicio 10 completado

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,18 +1,15 @@
-let year = 34992
-const leapYears = function() {
+//let year = 34992
+const leapYears = function(year) {
 // determinar una funcion que determine si un año es bisiesto o no
 // divisibles por cuatro %4 ? 
 //que no sea divisible por 100 
 //que no sean divisibles por 100 pero si de 400 
-if (year % 4 === 0) {
-    console.log(true)}
-if (year % 100 !==0 && year %400 ===0){
-    console.log(true)
-}
+if (year % 4 === 0 && (year % 100 !==0 || year %400 ===0)) {
+   return true }
  else{ 
-    console.log(false)
+    return false
 }
 }
-leapYears(year)
+//leapYears(year)
 // Do not edit below this line
 module.exports = leapYears;
