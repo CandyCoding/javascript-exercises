4f56d85ef1d71ed7857fb5132ef0c5a5581beb58
CandyL <candy.lopez.gr@gmail.com>
2026-06-19 17:40:00 -0600

    Ejercicio convertidor de temperaturas terminado

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let temp = (temperature - 32)*(5/9)
+  let rounded = Math.round(temp*10)/10
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+let tempF= (temperature * (9/5))+32
+let roundedF = Math.round (tempF*10)/10
+return roundedF
 };
 
 // Do not edit below this line
