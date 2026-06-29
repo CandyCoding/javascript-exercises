824262fbb3ec9ae8ae21a98107c6d6b1c8395552
CandyL <candy.lopez.gr@gmail.com>
2026-06-29 13:26:07 -0600

    ejercicio calculadora terminado

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+return array.reduce((acummulator,numero)=> acummulator + numero, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+return array.reduce((acummulator,numero)=> acummulator * numero ||0 +1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let resultado = 1 ;
+  for (let i = 2; i<= number; i++){
+    resultado = resultado * i
+  }
+  return resultado;
 };
 
 // Do not edit below this line
