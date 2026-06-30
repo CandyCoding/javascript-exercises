f1ba596315cafd5d8c1a8a97f9e30b9afbc638a0
CandyL <candy.lopez.gr@gmail.com>
2026-06-30 15:06:14 -0600

    fibonacci function done

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+let a= "4" 
 
-};
 
+const fibonacci = function(n) {
+n = Number(n)
+if (n === 0){
+   return 0
+}if (n < 0) {
+    return "OOPS"
+}
+ else ; 
+let previous = 0;
+let current = 1;
+for (let i= 1; i < n ; i ++){
+let next = previous + current
+previous = current;
+current = next;
+}
+return current
+}
+
+
+//console.log(fibonacci(a))
 // Do not edit below this line
 module.exports = fibonacci;
