02f05f5fd6beea464b5352ac56b38bb3afdbb92e
CandyL <candy.lopez.gr@gmail.com>
2026-06-30 13:36:20 -0600

    palindrome function terminada

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
 
+const word = 'A car, a man, a maraca.'
+const palindromes = function (string) {
+let minus =string.toLowerCase() // minusculas 
+let clean = minus.replace(/[^a-z0-9]/g,"")// hay que quitar espacios y signos
+console.log(clean)
+let reverse = clean.split("").reverse().join("")
+return reverse === clean
 };
-
 // Do not edit below this line
 module.exports = palindromes;
